ffc11564a4740dc1477c3389d5631e30445356d0
imsykim1130 <imsykim1130@gmail.com>
2025-08-12 15:26:29 +0900

    feat: Header1 구현 완료 및 문서화
    - Header1 컴포넌트 구현
    - README 에 공부한 내용 반영
    - 컴포넌트 css 작성

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # gsap 애니메이션 컴포넌트 모음
 
-gsap 라이브러리를 이용하여 애니메이션이 적용된 다양한 컴포넌트(header, footer, navigation...) 모음
+gsap 라이브러리를 이용하여 애니메이션이 적용된 다양한 컴포넌트(header, footer, navigation...) 구현 모음 및 배운 내용 정리
+
+## Header1
+
+### required package
+
+- gsap, @gsap/react
+- react-icons
+
+### What I Learned
+
+- gsap 의 fromTo 메서드 사용 시 특수 프로퍼티들(duration, delay, etc)은 from 에 넣지 말고 to 에 넣어야 한다.
---
+++ src/app/components/header/Header1.tsx
@@ -0,0 +1,134 @@
+"use client";
+import { useGSAP } from "@gsap/react";
+import { ReactNode, useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+import { IoLogoDribbble, IoLogoInstagram } from "react-icons/io";
+import { RxNotionLogo } from "react-icons/rx";
+import { MdArrowOutward } from "react-icons/md";
+import "./header1.css";
+
+const Header1 = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const nav = useRef<HTMLElement>(null);
+  const navTl = useRef<gsap.core.Timeline>(null);
+
+  useGSAP(
+    () => {
+      if (!nav.current) return;
+
+      // 자동으로 애니메이션이 실행되지 않고 네비게이션 토글 시에만 애니메이션 실행시키기 위해 paused 값 true 로 설정
+      const tl = gsap.timeline({ paused: true });
+
+      // 토글 시 애니메이션이 확실히 반대로 적용되도록 하기 위하여 fromTo 사용
+      tl.fromTo(
+        nav.current,
+        {
+          clipPath: "inset(0% 0% 100% 0%)",
+        },
+        {
+          clipPath: "inset(0% 0% 0% 0%)",
+          duration: 1.1,
+          ease: "power4.inOut",
+        }
+      ).fromTo(
+        ".menu",
+        {
+          opacity: 0,
+          y: "100%",
+          rotate: 30,
+        },
+        {
+          opacity: 1,
+          y: 0,
+          rotate: 0,
+          duration: 2,
+          ease: "power4.inOut",
+          stagger: 0.2,
+        },
+        "-=1.7"
+      );
+
+      navTl.current = tl;
+    },
+    { scope: nav }
+  );
+
+  //
+  useEffect(() => {
+    if (!navTl.current) return;
+
+    if (open) {
+      navTl.current.play();
+    } else {
+      navTl.current.reverse();
+    }
+  }, [open]);
+
+  return (
+    <header className="fixed top-0 left-0 w-full  px-10 md:px-30">
+      {/* navigation */}
+      <nav
+        ref={nav}
+        className={`absolute z-0 top-0 left-0 w-full h-screen flex flex-col md:flex-row justify-between hide-to-top bg-blue-300 pt-30 pb-10 px-10 md:px-50`}
+      >
+        <ul className="space-y-1 text-6xl md:text-7xl font-light md:min-w-90">
+          {menuList.map((menu) => (
+            <li
+              key={menu}
+              className="cursor-pointer w-full leading-14 tracking-tighter show transition-all duration-500 ease-in-out hover:translate-x-5"
+            >
+              <p className="menu origin-top-left">{menu}</p>
+            </li>
+          ))}
+        </ul>
+
+        <div>
+          <ul className="font-medium tracking-tight">
+            {socials.map((social) => (
+              <li key={social.name} className="social flex items-center gap-2">
+                <span>{social.icon}</span> {social.name} <MdArrowOutward />
+              </li>
+            ))}
+            <li className="mt-3">imsykim1130@gmail.com</li>
+          </ul>
+        </div>
+      </nav>
+
+      <div className="relative z-1 flex justify-between items-center h-20 font-medium">
+        {/* logo */}
+        <div>LOGO</div>
+
+        {/* menu/close button */}
+        <button
+          className="cursor-pointer"
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          {open ? "close" : "menu"}
+        </button>
+      </div>
+    </header>
+  );
+};
+
+const menuList: string[] = ["HOME", "WORK", "ABOUT", "CONTACT"];
+const socials: { name: string; icon?: ReactNode; link: string }[] = [
+  {
+    name: "instagram",
+    icon: <IoLogoInstagram />,
+    link: "#",
+  },
+  {
+    name: "dribble",
+    icon: <IoLogoDribbble />,
+    link: "#",
+  },
+  {
+    name: "notion",
+    icon: <RxNotionLogo />,
+    link: "#",
+  },
+];
+
+export default Header1;
